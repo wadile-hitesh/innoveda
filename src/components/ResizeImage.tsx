c4cfbf8fbc729513eabf1f2e1d6580639f6c99be
Hitesh Wadile <hiteshwadile2003@gmail.com>
2025-06-28 15:08:49 +0530

    feat: Resize Image Frontend Design

--- src/components/ResizeImage.tsx
+++ src/components/ResizeImage.tsx
@@ -1,35 +1,109 @@
-import axios from "axios";
-import { useState } from "react";
+import Image from "next/image";
+import { Checkbox } from "./ui/checkbox";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { Input } from "./ui/input";
+import { LockKeyhole, LockKeyholeOpen } from "lucide-react";
 
-export default function ResizeImage() {
-  const [imageUrl, setImageUrl] = useState("");
-  const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files ? e.target.files[0] : null;
-    if (!file) {
-      return;
-    }
 
-    const formData = new FormData();
-    formData.append("image", file);
+export default function ResizeImage(){
 
-    try {
-      const response = await axios.post("/api/resize-image", formData);
-      const base64Image = response.data.image;
-      const imageUrl = `data:image/png;base64,${base64Image}`;
-      setImageUrl(imageUrl);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
-    <main>
-      <div>
-        <p>Resize Image</p>
-        <div>
-          <input type="file" onChange={handleImageUpload} />
-          <img id="resized-img" alt="Resized Image" src={imageUrl} />
+    <div className="w-full flex gap-10 p-4 h-full">
+      <div className="w-5/7 h-full ">
+        <div className="h-full rounded-md bg-gray-800 p-6">
+          <div className="border-1 border-gray-600 h-full rounded-md">
+            <div className="flex flex-col items-center justify-center h-full">
+              <Image 
+                src="/images/upload-image.svg"
+                alt="Upload Image"
+                width={200}
+                height={200}
+                className=""
+              />
+              <p className="text-white font-mono text-xl">
+                Drop an image or click, <span className="text-orange-400">here</span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </main>
-  );
+      <div className="w-2/7 h-full px-3">
+        <div className="flex flex-col h-full justify-between ">
+          <div className="flex flex-col gap-10">
+            <div className="bg-gray-800 grid grid-cols-3 w-full gap-2 p-1 text-gray-300 font-bold text-sm rounded-md">
+              <div className="hover:bg-gray-900 rounded-md py-2 text-center">Pixel</div>
+              <div className="hover:bg-gray-900 rounded-md py-2 text-center">Percentage</div>
+              <div className="hover:bg-gray-900 rounded-md py-2 text-center">Social</div>
+            </div>
+            <div className="flex items-center justify-between w-full text-white text-md gap-4">
+              <div className="w-1/3">
+                <label>Width</label>
+                <Input type="number" defaultValue={1024} />
+              </div>
+          
+              <div className="flex items-end justify-center pb-3 gap-5 h-full w-1/3">
+                <LockKeyhole />
+                <LockKeyholeOpen />
+              </div>
+        
+              <div className="w-1/3">
+                <label>Height</label>
+                <Input type="number" defaultValue={1024} />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-5 items-center">
+            <div className="flex justify-between w-full text-white items-center">
+              <p className="text-md ml-2 text-white">
+                Save as
+              </p>
+              <div>
+              <Select defaultValue="jpeg">
+                <SelectTrigger className="w-[260px]">
+                  <SelectValue  className="text-white text-md ml-2" placeholder="JPEG" />
+                </SelectTrigger>
+                <SelectContent className="bg-gray-800">
+                  <SelectGroup className="text-white text-md ml-2 font-medium">
+                    <SelectItem value="jpeg">JPEG</SelectItem>
+                    <SelectItem value="png">PNG</SelectItem>
+                    <SelectItem value="webp">WEBP</SelectItem>
+                    <SelectItem value="svg">SVG</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              </div>
+            </div>
+
+            <div className="flex justify-between w-full text-white items-center">
+              <p className="text-md ml-2 text-white">
+                Compress
+              </p>
+              <div>
+              <Select defaultValue="no">
+                <SelectTrigger className="w-[260px]">
+                  <SelectValue  className="text-white text-md ml-2" placeholder="No Compression" />
+                </SelectTrigger>
+                <SelectContent className="bg-gray-800">
+                  <SelectGroup className="text-white text-md ml-2 font-medium">
+                    <SelectItem value="high">High Compression</SelectItem>
+                    <SelectItem value="medium">Medium Compression</SelectItem>
+                    <SelectItem value="low">Light Compression</SelectItem>
+                    <SelectItem value="no">No Compression</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              </div>
+            </div>
+            
+            <div className="flex gap-2 items-center text-white">
+              <Checkbox className="data-[state=checked]:border-amber-600 data-[state=checked]:bg-amber-600 data-[state=checked]:text-white dark:data-[state=checked]:border-amber-700 dark:data-[state=checked]:bg-amber-700"/>
+              <label className="text-md ml-2 text-white">Set Max Size</label>
+            </div>
+            <button className=" w-full text-center px-1 py-2 rounded-md bg-amber-700 font-md text-base">Resize</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
