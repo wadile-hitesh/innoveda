270a44b23e097e2a67d68250ee950d8b59973692
Hitesh Wadile <hiteshwadile.dev@gmail.com>
2025-07-21 10:44:36 +0530

    feat:convert from jpg completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900`}
       > 
-        <Toaster position="bottom-right" richColors/>
+        <Toaster position="top-right" richColors/>
         {children}
       </body>
     </html>
