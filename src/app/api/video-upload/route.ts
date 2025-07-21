ec25c1c36da334e4c839a3a7e18a95c2ad762a81
Hitesh Wadile <hiteshwadile.dev@gmail.com>
2025-07-21 19:54:07 +0530

    Sitemap xml added

--- src/app/api/video-upload/route.ts
+++ src/app/api/video-upload/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { v2 as cloudinary } from 'cloudinary';
 
-
-
 cloudinary.config({
     cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
     api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
