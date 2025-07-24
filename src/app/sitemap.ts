1a0a52c51838d4f4280c2ed9571525f6d492d1a9
Hitesh Wadile <hiteshwadile.dev@gmail.com>
2025-07-24 06:50:04 +0530

    Fixing canonical Name

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap{
     return [
         {
-            url : 'https://innoveda.tech',
+            url : 'https://www.innoveda.tech',
             lastModified : new Date(),
             changeFrequency : 'monthly',
             priority : 1,
         },
         {
-            url : 'https://innoveda.tech/social-share',
+            url : 'https://www.innoveda.tech/social-share',
             lastModified : new Date(),
             changeFrequency : 'weekly',
             priority : 0.8,
         },
         {
-            url : 'https://innoveda.tech/convert-from-jpg',
+            url : 'https://www.innoveda.tech/convert-from-jpg',
             lastModified : new Date(),
             changeFrequency : 'weekly',
             priority : 0.8,
         },
         {
-            url : 'https://innoveda.tech/background-remover',
+            url : 'https://www.innoveda.tech/background-remover',
             lastModified : new Date(),
             changeFrequency : 'weekly',
             priority : 0.8,
