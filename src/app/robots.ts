b31e956c79590c5eda13b216ef511ff51870784d
Hitesh Wadile <hiteshwadile.dev@gmail.com>
2025-07-24 07:33:30 +0530

    Canonical www removed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://www.innoveda.tech/sitemap.xml',
+    sitemap: 'https://innoveda.tech/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap{
     return [
         {
-            url : 'https://www.innoveda.tech',
+            url : 'https://innoveda.tech',
             lastModified : new Date(),
             changeFrequency : 'monthly',
             priority : 1,
         },
         {
-            url : 'https://www.innoveda.tech/social-share',
+            url : 'https://innoveda.tech/social-share',
             lastModified : new Date(),
             changeFrequency : 'weekly',
             priority : 0.8,
         },
         {
-            url : 'https://www.innoveda.tech/convert-from-jpg',
+            url : 'https://innoveda.tech/convert-from-jpg',
             lastModified : new Date(),
             changeFrequency : 'weekly',
             priority : 0.8,
         },
         {
-            url : 'https://www.innoveda.tech/background-remover',
+            url : 'https://innoveda.tech/background-remover',
             lastModified : new Date(),
             changeFrequency : 'weekly',
             priority : 0.8,
